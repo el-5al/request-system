7ccfff32669021a873d5a6a76c48eb2bfe3149f7
el-5al <49070541+el-5al@users.noreply.github.com>
2026-03-15 04:50:07 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Request(BaseModel):
+    username: str
+    email: str
+    body: str
 
 @app.get("/")
 def home():
     return {"status": "request system running"}
+
+@app.post("/create_request")
+def create_request(req: Request):
+    return {
+        "message": "request received",
+        "user": req.username,
+        "body": req.body
+    }
